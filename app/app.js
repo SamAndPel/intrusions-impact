04f3b4b271efe0560d6752e5e34423287ce177f6
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2021-08-06 14:15:01 +0100

    Add basic nodeJS boilerplate

---
+++ app/app.js
@@ -0,0 +1,27 @@
+// INITIAL PROGRAM SETUP ------------------------------------------------------
+// Import and configure required libraries
+
+const express = require("express");
+const app = express();
+const nunjucks = require("nunjucks");
+const dotenv = require("dotenv");
+
+app.use(express.static(__dirname + "/static/"));
+nunjucks.configure(__dirname + "/templates/", {
+    autoescape: true,
+    express: app
+});
+
+// Set port to .env variable value if found, else set port to 3000
+const PORT = process.env.PORT || 3000;
+
+// ROUTES ---------------------------------------------------------------------
+app.get("/", (req, res) => {
+    console.log("returned game instance");
+    return res.render("game.html.njk");
+});
+
+// HOST HTTP SERVER -----------------------------------------------------------
+app.listen(PORT, () => {
+    console.log("[+] Server listening on port " + PORT);
+});
