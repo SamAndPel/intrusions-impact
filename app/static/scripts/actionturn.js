2de2f63bdcd5e28d008b548740f60d72bc829a0d
Sam Anderson-Peled <12894624+SamAndPel@users.noreply.github.com>
2021-08-17 12:54:23 +0100

    Correct consequences are pulled into modal

--- app/static/scripts/actionturn.js
+++ app/static/scripts/actionturn.js
@@ -24,7 +24,8 @@ function doturn() {
                 conseqs.push(card.consequences[turn].notinstalled);
             }
         });
-        // TODO: Prune empty consequences from conseqs[] data structure before continuing
+        // Prune empty consequences from list
+        conseqs = conseqs.filter(conseq => (conseq["text"]));
 
         // Action results of consequences on score etc
         conseqs.forEach(cons => {
@@ -88,8 +89,61 @@ function validatecards(stagedcards) {
 
 // Renders a consequences modal
 function showconsequences(consequencelist, callback) {
-    console.log("[+] Consequence modal rendered, waiting for user input");
+    // Generate HTML for each consequence in list and append it to the modal
+    // EXAMPLE -------------------------------------------------------------------
+    // <p class="info-body-text"><b>Here are the consequences of your actions</b></p>
+    // <p class="info-body-text">Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! </p>
+    // <div class="modallinkspace">
+    //     <a href="google.co.uk" class="modallink" target="_blank">Read more</a>
+    //     <a href="google.co.uk" class="modallink" target="_blank">Example details</a>
+    // </div>
+    // <p class="info-body-text">Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! </p>
+    // <div class="modallinkspace">
+    //     <a href="google.co.uk" class="modallink" target="_blank">Read more</a>
+    //     <a href="google.co.uk" class="modallink" target="_blank">Example details</a>
+    // </div>
+    // <p class="info-body-text">Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! Here's a whole lot of consequence text blah blah blah don't read me I repeat! </p>
+    // <div class="modallinkspace">
+    //     <a href="google.co.uk" class="modallink" target="_blank">Read more</a>
+    //     <a href="google.co.uk" class="modallink" target="_blank">Example details</a>
+    // </div>
+    // <div class="modallinkspace">
+    //     <p id="conseqcontinue" class="modallink">Continue</p>
+    // </div>
+
+    let modalroot = document.getElementById("conseqmodalroot");
+
+    // Wipe existing content in the consequence modal
+    modalroot.innerHTML = ""
+
+    // Create the stinger at the top
+    let stinger = document.createElement("p");
+    let embolden = document.createElement("b");
+    let stingertext = document.createTextNode("Here are the consequences of your actions:");
+    embolden.appendChild(stingertext);
+    stinger.appendChild(embolden);
+    modalroot.appendChild(stinger);
+
+    consequencelist.forEach(conseq => {
+        const consdiv = document.createElement("div");
+        consdiv.classList.add("consequence-div");
+        consdiv.innerHTML = JSON.stringify(conseq);
+        modalroot.appendChild(consdiv);
+    });
+
+    // Create the continue button
+    let contbtndiv = document.createElement("div");
+    contbtndiv.classList.add("modallinkspace");
+    let contbtn = document.createElement("p");
+    contbtn.classList.add("modallink");
+    contbtn.id = "conseqcontinue";
+    let contbtntext = document.createTextNode("Continue");
+    contbtn.appendChild(contbtntext);
+    contbtndiv.appendChild(contbtn);
+    modalroot.appendChild(contbtndiv);
+
     document.getElementById("conseqmodal").style.display = "block";
+    console.log("[+] Consequence modal rendered, waiting for user input");
 
     // Only trigger the callback when the user clicks continue
     let continuebtn = document.getElementById("conseqcontinue");
